370b70d82732d13edaa0a57563cb753172683c63
Jonathan Cripps <jcripps@SCLTMAC-10080.local>
2019-01-14 10:51:46 -0500

    Module 4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,37 +1,43 @@
 <template>
+  <div>
     <h1>{{ title }}</h1>
     <ul>
-        <li v-for="book in books">{{ book.title }} : {{ book.author }} </li>
+      <book-item v-for="book in books" :book='book'></book-item>
     </ul>
+  </div>
 </template>
 
 <script>
+import BookItem from "/components/BookItem";
 export default {
-    name: 'BookList',
-    data() {
-        return {
-        title: 'All Books',
-        books:
-        [
-            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-            {title: 'American Gods', author: 'Neil Gaiman'},
-            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-        ]
-        };
-    }
-}
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
+      ]
+    };
+  },
+  components: {
+    BookItem
+  }
+};
 </script>
 
 <style>
-    h1, h2 {
-        font-weight: normal;
-    } 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    li {
-        display: block;
-        margin: 0 10px;
-    }
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
 </style>
