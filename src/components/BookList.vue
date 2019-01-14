6844260a86fc199d3fd59f808cf253ccfc50e557
Jonathan Cripps <jcripps@SCLTMAC-10080.local>
2019-01-14 09:12:07 -0500

    Adding BookList.vue

---
+++ src/components/BookList.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'BookList',
+    data() {
+        return {
+        Title: 'All Books',
+        };
+    }
+}
+</script>
+
+<style>
+    h1, h2 {
+        font-weight: normal;
+    } 
+</style>
