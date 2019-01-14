433bb5c0fbd33baca75e250c30847391cac37b43
Jonathan Cripps <jcripps@SCLTMAC-10080.local>
2019-01-14 10:07:54 -0500

    Fixing missing h1 tag in BookList

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,5 +1,5 @@
 <template>
-    
+    <h1>{{ title }}</h1>
 </template>
 
 <script>
@@ -7,7 +7,7 @@ export default {
     name: 'BookList',
     data() {
         return {
-        Title: 'All Books',
+        title: 'All Books',
         };
     }
 }
